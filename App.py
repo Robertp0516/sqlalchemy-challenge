a0a6005acbbd84d11b1f7cbe0d252c8b5ff2c241
Robert Peron <Robertp0516@gmail.com>
2020-08-19 21:53:35 -0400

    Flask App Added - Final Push

--- App.py
+++ App.py
@@ -1,38 +1,104 @@
-#1. Import Flask and Modules used in Jupyter Notebook 
-from flask import Flask, jsonify
-
-import sqlite3
-import datetime as dt
-import numpy as np
-
+# Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, inspect
 
-#2. Database Setup & reflect for tables
-engine = create_engine("sqlite:////Users\rober\OneDrive\Desktop\BootCamp HW Folder\My Homework Submission\sqlalchemy-challenge\Resources\hawaii.sqlite")
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
-
 Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 session = Session(engine)
-# Create our session (link) from Python to the DB
-measurements = Base.classes.measurement
-stations = Base.classes.station
 
-#3. Create app, passing __name__
 app = Flask(__name__)
 
-#3. Define what to do when a user hits index route
 @app.route("/")
-def weclome():
-    print('Welcome to my "Home" page! Here are the available API Routes for Surfs Up:')
+def home():
+    """Home Page: Here are the Available API Routes for Surfs Up"""
     return (
-        f"Available API Routes:<br/>"
-        f"/api/v1.0/precipitation:<br/>"
-        f"/api/v1.0/stations:<br/>"
-        f"/api/v1.0/tobs:<br/>"
-        f"/api/v1.0/<start>:<br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start/end<br/>"
     )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    latest_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    one_year_date = (dt.datetime.strptime(latest_date[0],'%Y-%m-%d') - dt.timedelta(days=365)).strftime('%Y-%m-%d')
+    query_cols = (measurement.date, measurement.prcp)
+    year_data = session.query(*query_cols).filter(measurement.date >= one_year_date).all()
+    
+    precip_dict = []
+    for result in year_data:
+        r = {}
+        r[result[0]] = result[1]
+        precip_dict.append(r)
+    return jsonify(precip_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    stations = session.query(station.station, station.name).all()
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def observations():
+    session = Session(engine)
+    latest_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    one_year_date = (dt.datetime.strptime(latest_date[0],'%Y-%m-%d') - dt.timedelta(days=365)).strftime('%Y-%m-%d')
+    year_data = session.query(measurement.date, measurement.tobs).\
+            filter(measurement.station == 'USC00519281').\
+            filter(measurement.date > one_year_date).all()
+    return jsonify(year_data)
+
+@app.route("/api/v1.0/temp/start")
+def start_dt(start_dt):
+    session = Session(engine)
+    start = dt.datetime.strptime(start_dt, '%Y-%m-%d')
+    results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+                filter(measurement.date >= start).all()
+    start_list = []
+    for c in results:
+        c = {}
+        c["Start Dt"] = start
+        c["Temp_Min"] = results[0]
+        c["Temp Max"] = results[1]
+        c["Temp Avg"] = results[2]
+        start_list.append(c)
+    return jsonify(start_list)
+
+@app.route("/api/v1.0/temp/start/end")
+def start_end(start_dt,end_dt):
+        session = Session(engine)
+        start = dt.datetime.strptime(start_dt, '%Y-%m-%d')
+        end = dt.datetime.strptime(end_dt, '%Y-%m-%d')
+        results = session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
+                filter(measurement.date >= start).\
+                filter(measurement.date <= end).all()
+        result_list = []
+        for c in results:
+            c = {}
+            c["Start Dt"] = start
+            c["End Dt"] = end
+            c["Temp_Min"] = results[0]
+            c["Temp Max"] = results[1]
+            c["Temp Avg"] = results[2]
+            result_list.append(c)
+        return jsonify(result_list)
+
+if __name__ == "__main__":
+    app.run(debug=True)
